bf55da64acd65f1e5933813b8bc71dbcddde933e
goldovskaya-m <ariya-aaa@yandex.ru>
2025-03-15 18:17:39 +0300

    Initial commit

---
+++ src/main/java/sky/pro/Hogwarts31Test/Hogwarts31TestApplication.java
@@ -0,0 +1,13 @@
+package sky.pro.Hogwarts31Test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hogwarts31TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hogwarts31TestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sky/pro/Hogwarts31Test/Hogwarts31TestApplicationTests.java
@@ -0,0 +1,13 @@
+package sky.pro.Hogwarts31Test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hogwarts31TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
