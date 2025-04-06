e9cfddd5c0cc4dfc0e14f76d6495a3a9967665e1
goldovskaya-m <ariya-aaa@yandex.ru>
2025-04-06 17:32:18 +0300

    commit Во все методы сервисов добавлено логирование. В application.properties создано свойство server.port = 8080. Создан класс-контроллер InfoController. В InfoController создан метод GET /port , который возвращает значение проперти server.port. В проекте создано минимум 2 конфигурационных файла с разными значениями проперти server.port. Логи несут в себе полезную и понятную информацию. Logger сделан private static final. Корректно подключен Swagger. При выполнении цикла CRUD-операций (POST, GET, PUT, DELETE) не возвращается статус ответа 200. Соблюден паттерн MVC. Созданы пакеты model, serivce, controller. HTTP-методы используются по правилам построения REST-запросов. В контроллере есть только метод вызова сервиса и нет лишней логики. Коды HTTPответов соответствуют их семантике. Соблюдена инкапсуляция. Название метода начинается с глагола и описывает действия метода. Применяется принцип camelCase. Код успешно комплируется, и каждое действие расположено на отдельной строке. Использовано автоматическое форматирование кода. В репозитории нет лишних файлов. Переданная ссылка — это pull request. Отсутствуют лишние блоки и переменные. Имена классов начинаются с большой буквы. Нет двойной вложенности. Соблюдена конвенциональная структура класса: поля, конструкторы и методы. Полям присвоены имена, которые отражают их суть. Каждый класс находится в отдельном файле. Классы корректно распределены по пакетам. Отсутствуют лишние нефункциональные комментарии. Отсутствуют методы, оперирующие raw type. Повторяющийся хардкод вынесен в константы. Отсутсвуют методы, чья логика может вернуть null. URL-маппинг прописан по правилам построения REST-запросов. Эндпойнты семантически подходят к контроллерам, в которых они реализованы. Контроллеры реализованы без использования класса ResponseEntity (кроме выгрузки файлов).

--- src/main/java/sky/pro/Hogwarts31Test/service/StudentServiceImpl.java
+++ src/main/java/sky/pro/Hogwarts31Test/service/StudentServiceImpl.java
@@ -1,5 +1,7 @@
 package sky.pro.Hogwarts31Test.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -15,6 +17,7 @@ import java.util.Collection;
 @Service
 public class StudentServiceImpl implements StudentService {
     private final StudentRepository studentRepository;
+    private final Logger logger = LoggerFactory.getLogger(StudentServiceImpl.class);
 
     public StudentServiceImpl(StudentRepository studentRepository) {
         this.studentRepository = studentRepository;
@@ -22,14 +25,17 @@ public class StudentServiceImpl implements StudentService {
 
     @Override
     public Long add(Student student) {
-
-        if (studentRepository.existsById(student.getId())){
-            throw new RuntimeException(("Студент уже существует"));}
+        logger.info("Вызван метод создания объекта студент");
+        if (studentRepository.existsById(student.getId())) {
+            logger.error("Студент уже существует");
+            throw new RuntimeException("Студент уже существует");
+        }
         return studentRepository.save(student).getId();
     }
 
     @Override
     public Student update(Long id, Student student) {
+        logger.info("Вызван метод обновления объекта студента с id {}", id);
         checkStudentExist(id);
         student.setId(id);
         return studentRepository.save(student);
@@ -37,8 +43,8 @@ public class StudentServiceImpl implements StudentService {
 
     @Override
     public Student deleteById(Long id) {
+        logger.info("Вызван метод удаления объекта студент");
         checkStudentExist(id);
-        // return repository.remove(id);
         studentRepository.deleteById(id);
         return null;
 
@@ -46,56 +52,61 @@ public class StudentServiceImpl implements StudentService {
 
     @Override
     public Student findById(Long id) {
+        logger.info("Вызван метод поиска объекта студент");
         return studentRepository.findById(id)
                 .orElseThrow(() -> new StudentNotFoundException(id));
     }
 
     @Override
     public Collection<Student> FindAll() {
+        logger.info("Вызван метод поиска всех объектов студент");
         return (studentRepository.findAll());
 
     }
 
     @Override
     public Collection<Student> FindByAge(int age) {
-
-
+        logger.info("Вызван метод поиска объекта студент по возрасту");
         return studentRepository.findAll().stream()
                 .filter(student -> student.getAge() == age).toList();
     }
 
     @Override
     public Collection<Student> findByAgeBetween(int min, int max) {
+        logger.info("Вызван метод поиска объектов студент по возрасту в промежутке мин-макс");
         return studentRepository.findByAgeBetween(min, max);
     }
 
     @Override
     public Long checkStudentExist(Long id) {
+        logger.info("Вызван метод проверки наличия объекта студент");
         checkStudentExist(id);
         {
             if (!studentRepository.existsById(id)) {
+                logger.error("Студент не найден");
                 throw new StudentNotFoundException(id);
             } else {
                 return id;
             }
 
         }
-
     }
 
     @Override
     public long countAllStudents() {
+        logger.info("Вызван метод подсчета объектов студент");
         return studentRepository.countAllStudents();
     }
 
     @Override
     public Double getAverageAgeStudent() {
+        logger.info("Вызван метод подсчета среднего возраста объектов студент");
         return studentRepository.getAverageAgeStudent();
     }
 
     @Override
-    @Transactional
     public Page<Student> fiveLastStudents() {
+        logger.info("Вызван метод поиска последних пяти объектов студент");
         Pageable pageable = PageRequest.of(0, 5);
         return studentRepository.findLastFiveStudents(pageable);
     }
